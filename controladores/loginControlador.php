6b1f3b9c5ebaf56bb9421d4a1d96b0fa7bc2aa1d
Enrique <cartelenrique18@gmail.com>
2022-09-14 15:30:02 -0500

    Prueba de version

---
+++ controladores/loginControlador.php
@@ -0,0 +1,11 @@
+<?php
+
+	if($peticionAjax){
+		require_once "../modelos/loginModelo.php";
+	}else{
+		require_once "./modelos/loginModelo.php";
+	}
+
+	class loginControlador extends loginModelo{
+
+	}
